346c9beca889af0c29b057806a20083c6afe014e
Andreas Eriksson <andreas@codered.se>
2026-05-21 11:31:05 +0200

    Landing page

--- test/skill_pot_web/controllers/page_controller_test.exs
+++ test/skill_pot_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule SkillPotWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+    assert html_response(conn, 200) =~ "One skill library."
   end
 end
